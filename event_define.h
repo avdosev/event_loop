3857614a6d864afb17359601868a17bf437a336e
kyplyk <avdosev1@yandex.ru>
2019-12-31 14:15:43 +0400

    move releasation to cpp

--- event_define.h
+++ event_define.h
@@ -10,8 +10,4 @@ using event_t = std::shared_ptr<Event>;
 using callback_t = std::function<void(void)>;
 using tiker_t = std::function<uint32_t(void)>;
 
-// std::pair<event_id_t, event_t>
-using event_pair = struct {
-    event_id_t first;
-    event_t second;
-};
+using event_pair = std::pair<event_id_t, event_t>;
